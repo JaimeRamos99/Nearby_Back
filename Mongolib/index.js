c6946913ae65648ee48e34cc3c6f4442a889e128
JaimeRamos99 <jrossetes@uninorte.edu.co>
2019-11-27 12:39:59 -0500

    done!

---
+++ Mongolib/index.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
+class Mongohandler {
+    constructor() {
+        this.client = new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true });
+        this.dbName = 'nearby-beta'
+    }
+    connect() {
+        return new Promise((resolve, reject) => {
+            this.client.connect(err => {
+                if (err) {
+                    reject(err)
+                }
+                console.log('Connected')
+                resolve(this.client.db(this.dbName))
+            })
+        })
+    }
+    async nuevoUsuario(name, email, user, pass) {
+        return this.connect().then(db => {
+            return db.collection('users').insertOne({ nombre: name, correo: email, usuario: user, contraseña: pass });
+        })
+    }
+}
+module.exports = new Mongohandler()
--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const firebaseConfig = require('./Config')
 const firebase = require("firebase/app");
+const bcrypt = require('bcrypt')
+const cors = require("cors");
+const corsOptions = { origin: "http://localhost:3000" };
+const mongohandler = require('./Mongolib')
 require("firebase/auth");
 require("firebase/firestore");
 app.listen(3001);
 app.use(bodyParser.json())
+app.use(cors(corsOptions));
 app.use(router)
 console.log('server started on port 3001');
 firebase.initializeApp(firebaseConfig);
 router.post('/createUserEmailPassword/:nombre/:email/:usuario/:password', async function (req, res) {
     console.log("se conectaron a /createUserEmailPassword/:nombre/:email/:usuario/:password")
+    let flag = false
     let email = req.params.email
     let password = req.params.password
+    const hashedpassword = await bcrypt.hash(password, 10)
     let nombre = req.params.nombre
+    let user = req.params.usuario
     await firebase.auth().createUserWithEmailAndPassword(email, password).then(result => {
         result.user.updateProfile({
             displayName: nombre
@@ -24,19 +32,28 @@ router.post('/createUserEmailPassword/:nombre/:email/:usuario/:password', async 
             url: 'http://localhost:3001/'
         }
         result.user.sendEmailVerification(configuracion).catch(err => {
-            console.log(err)
+            res.json("1")
+            console.log("1:" + err)
+            flag = true
         })
         firebase.auth().signOut()
     }).catch(err => {
-        console.error(err)
+        flag = true
+        console.error("2:" + err)
+        res.json(err)
     })
-    res.json("Done!")
+    
+    if (!flag) {
+        await mongohandler.nuevoUsuario(nombre, email, user, hashedpassword)
+        res.json("Done!")
+    }
+
 })
 router.get('/login/:email/:password', async function (req, res) {
     console.log("se conectaron a /login/email/password")
     let email = req.params.email
     let password = req.params.password
-    firebase.auth().signInWithEmailAndPassword(email, password).then(result => {//la contraseña si se guarda de algún extraño modo XD
+    firebase.auth().signInWithEmailAndPassword(email, password).then(result => {//la contraseña si se guarda de algún extraño modo XD para cuando vaya a dar sign in
         if (result.user.emailVerified) {
             res.json("ingreso exitoso!")
         } else {
